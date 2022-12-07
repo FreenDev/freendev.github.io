1275d9a4fcff0f4862313a4ec59a9775602f5c65
FreenDev <freendev@mail.ru>
2022-12-07 11:45:16 +0300

    upload site

---
+++ JS/connect-to-websocket.js
@@ -0,0 +1,14 @@
+var socket;
+window.onload=function(){
+   socket=io.connect("https://b22e-136-169-214-0.eu.ngrok.io");
+   socket.on('connect', function() {
+      socket.emit('cev', 'Android');
+      console.log("Соединение есть!");
+   });
+   socket.on('disconnect', function() {
+      console.log('Соединение пропало!');
+   });
+   document.getElementById('forward').addEventListener("click", function() {
+      socket.emit('sev', "forward");
+   });
+}
